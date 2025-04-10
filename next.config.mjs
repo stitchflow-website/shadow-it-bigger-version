7f5cb9c4516f5bbf105a492923d3131889f4c840
ThamimTommy-stitchflow <thamim.tommy@stitchflow.io>
2025-04-10 18:29:55 +0530

    Add assetPrefix to Next.js configuration for shadow IT tools

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,7 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+  assetPrefix:'/tools/shadow-it',
 }
 
 function mergeConfig(nextConfig, userConfig) {
