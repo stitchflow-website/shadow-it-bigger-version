a645fb9476ea17972acfb21f6fe8b8258cb94ced
ThamimTommy-stitchflow <thamim.tommy@stitchflow.io>
2025-06-04 11:28:34 +0530

    Add Google Tag Manager integration to layout component
    - Included Google Tag Manager script in the head section for enhanced tracking and analytics.
    - Added noscript fallback for users with JavaScript disabled, ensuring tracking continuity.
    - Updated meta tags and favicon for improved SEO and branding consistency.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import Link from 'next/link';
 import { Suspense } from 'react';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Free Shadow IT Scanner for SaaS & AI Apps | Stitchflow',
@@ -16,7 +17,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K8ZP8PLV');`
+          }}
+        />
+        {/* End Google Tag Manager */}
+
+        <meta charSet="UTF-8" />
+        <link rel="icon" type="image/png" href="https://cdn.prod.website-files.com/648b3fb5ff20b9eb641b8ea2/65b1d6fd463f7ec764b594e6_Group%207.png" />
+        <link rel="canonical" href="https://www.stitchflow.com/tools/shadow-it-scan" />
+      </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K8ZP8PLV"
+        height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Suspense fallback={<div>Loading...</div>}>
           {children}
         </Suspense>
